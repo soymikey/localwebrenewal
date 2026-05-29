f7d39d9d13a9c7bd1c64edcb024dece69f521d3d
Michael ChengMQ <soymikey0717@gmail.com>
2026-05-29 19:27:34 -0400

    Move HVAC demo to client-specific path

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         demo: resolve(__dirname, "demo/index.html"),
+        newYorkHeating: resolve(__dirname, "demo/new-york-heating/index.html"),
       },
     },
   },
